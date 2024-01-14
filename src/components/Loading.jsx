3ca2d473aa90fd09f43141fcdd4a421ec50a5c5e
dattecode <dattecode@gmail.com>
2024-01-14 19:51:24 -0300

    loading smoth

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -8,7 +8,7 @@ const Loading = () => {
       <motion.article
         className="loadingCont"
         initial={{ opacity: 0, y: -30 }}
-        animate={{ opacity: 1, y: 0, transition: { duration: 0.05 } }}
+        animate={{ opacity: 1, y: 0, transition: { duration: 0.5 } }}
         exit={{ opacity: 0 }}
       >
         <h3 className="loadingMess">Cargando.. 😉</h3>
